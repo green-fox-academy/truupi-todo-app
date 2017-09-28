9517ef48c82a877a636e9abdf2c50d0eb465a694
Tamas <deaktamas93@gmail.com>
2017-09-28 10:45:33 +0200

    feat(program): print usage main screen added

---
+++ ToDoApp/ToDoApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome();
+        }
+
+        static void Welcome()
+        {
+            Console.WriteLine("\nCommand Line Todo Application\n" + "=============================\n\n" + "Command line arguments:\n" + "-l   Lists all the tasks\n" +
+                "-a   Adds a new task\n" + "-r   Removes a task\n" + "-c   Completes a task\n");
+            Console.ReadLine();
+        }
+    }
+}
