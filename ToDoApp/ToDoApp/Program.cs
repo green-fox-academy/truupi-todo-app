b01ba7bdd7bdde652ce5cc7465f76a008e6f12a8
Tamas <deaktamas93@gmail.com>
2017-09-28 18:17:25 +0200

    refactoring(program)

--- ToDoApp/ToDoApp/Program.cs
+++ ToDoApp/ToDoApp/Program.cs
@@ -9,30 +9,38 @@ namespace ToDoApp
 {
     class Program
     {
+        static TaskList tasking = new TaskList();
+
         static void Main(string[] args)
         {
-            TaskList tasking = new TaskList();
-
-            if (args.Contains("-l"))
+            if (args.Length == 0)
+            {
+                ArgumentsList();
+            }
+            else if (args.Contains("-l"))
             {
                 tasking.ListTasks();
             }
             else if (args.Contains("-a"))
             {
-                tasking.AddTask(args[1]);
+                    tasking.AddTask(args);
             }
             else if (args.Contains("-r"))
             {
                 tasking.RemoveTask(args[1]);
             }
+            else if (args.Contains("-f"))
+            {
+                tasking.CheckTask();
+            }
             else
             {
-                ReadingArgmnts();
-                Console.ReadLine();
+                Console.WriteLine("Unsupported argument!");
             }
+            Console.ReadLine();
         }
 
-        static void ReadingArgmnts()
+        static void ArgumentsList()
         {
             string path = @"..\..\assets\welcomemsg.txt";
             Console.WriteLine(File.ReadAllText(path));
--- ToDoApp/ToDoApp/TaskList.cs
+++ ToDoApp/ToDoApp/TaskList.cs
@@ -12,18 +12,45 @@ namespace ToDoApp
         static string path = @"..\..\assets\todolist.txt";
         static string[] taskArray = File.ReadAllLines(path);
 
+        string emptyBox = "[ ]";
+        string checkedBox = "[x]";
+
         internal void ListTasks()
         {
-            switch (taskArray.Length)
+            if (taskArray.Length == 0)
             {
-                case 0:
-                    Console.WriteLine("No todos for today! :)");
-                    break;
-                default:
-                {
-                    WriteTasksToConsole();
-                    break;
-                }
+                Console.WriteLine("No todos for today! :)");
+            }
+            else
+            {
+                WriteTasksToConsole();
+            }
+        }
+
+        internal void RemoveTask(string argsPlace)
+        {
+            var taskList = new List<string>(System.IO.File.ReadAllLines(path));
+            if (taskList.Count >= 2)
+            {
+                taskList.RemoveAt(Convert.ToInt32(argsPlace) - 1);
+                File.WriteAllLines(path, taskList);
+            }
+        }
+
+        internal void CheckTask()
+        {
+            
+        }
+
+        internal void AddTask(string[] args)
+        {
+            if (args.Count() == 1)
+            {
+                Console.WriteLine("Unable to add: no task provided");
+            }
+            else
+            {
+            using (StreamWriter taskWriter = File.AppendText(path)) { taskWriter.WriteLine("{0} {1}", emptyBox, args[1]); }
             }
         }
 
@@ -33,20 +60,6 @@ namespace ToDoApp
             {
                 Console.WriteLine("{0} - {1}", i + 1, taskArray[i]);
             }
-        }
-
-        internal void RemoveTask(string taskPlace)
-        {
-            var taskList = new List<string>(System.IO.File.ReadAllLines(path));
-            taskList.RemoveAt(Convert.ToInt32(taskPlace) - 1);
-
-            File.WriteAllLines(path, taskList);
-        }
-
-        internal void AddTask(string inputTask)
-        {
-            using (StreamWriter taskWriter = File.AppendText(path))
-                { taskWriter.WriteLine(inputTask); }
         }
     }
 }
